72504a8cafdf7c527ca50ccf15a86db2265bf271
yukai <83336281+yukailin-git@users.noreply.github.com>
2023-08-31 16:49:37 +0800

    sixth

--- puppy.go
+++ puppy.go
@@ -22,6 +22,10 @@ func From10() {
 	fmt.Println("I'm from version 1.0.0")
 }
 
-func From11(){
+func From11() {
 	fmt.Println("I'm from version 1.2.0")
+}
+
+func From12() {
+	fmt.Println("I'm from version 1.3.0")
 }
